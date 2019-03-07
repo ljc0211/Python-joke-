4a56835955021ed357feb96a80d3586e8808acca
ljc0211 <757818798@qq.com>
2019-03-07 15:56:55 +0800

    Edit in 2019.3.7

--- demo.py
+++ demo.py
@@ -1,20 +1,51 @@
-array = ["gkd", "ahhhhh!", "www"]
+# 2018/3/5
+# array = ["gkd", "ahhhhh!", "www"]
+#
+# print(array)
+#
+# array.append("wocao")
+# array.append("wocao")
+# array.append("wocao")
+#
+# array.remove("wocao")
+#
+# array.extend(["产业", "孙利龙","利龙大神真TM太帅了啊"])
+#
+# def print_each(the_list):
+#     for each_item in the_list:
+#         if(isinstance(each_item, list)):
+#             print_each(each_item)
+#         else:
+#             print(each_item)
+#
+# print_each(array)
 
-print(array)
+# 2018/3/6
+# array = ["中国", "美国", 1999, 2001, ["code", "complier"]]
+#
+# print(array)
+#
+# rows = 3
+# cols = 3
+# matrix = [[0 for col in range(cols)] for row in range(rows)]
+# for i in range(rows):
+#     for j in range(cols):
+#         matrix[i][j] = i * 3 + j
+#         print(matrix[i][j], end = ",")
+#     print('\n')
+#
+#
+#     print(x*x for x in range(1,11))
+#
 
-array.append("wocao")
-array.append("wocao")
-array.append("wocao")
+# # 元组操作
+# tup1 = (12, 34, 66)
+# tup2 = (78, 90)
+#
+# tup3 = tup1 + tup2
+#
+# print(tup3)
+#
+# tup2 = (90, 78)
+# print(tup2)
 
-array.remove("wocao")
-
-array.extend(["产业", "孙利龙","利龙大神真TM太帅了啊"])
-
-def print_each(the_list):
-    for each_item in the_list:
-        if(isinstance(each_item, list)):
-            print_each(each_item)
-        else:
-            print(each_item)
-
-print_each(array)
